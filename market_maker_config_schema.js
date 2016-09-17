1e82ae2b5ef99c26d51aec2b8ac25d72e8ad3b80
etherdelta <etherboost+etherdelta@gmail.com>
2016-09-17 11:50:45 -0500

    update

--- market_maker_config_schema.js
+++ market_maker_config_schema.js
@@ -2,54 +2,131 @@
 
 var schema = {
   "$schema": "http://json-schema.org/draft-04/schema#",
-  "type": "object",
-  "additionalProperties": false,
-  "properties": {
-    "pairs": {
-      "type": "array",
-      "items": {
-        "type": "object",
-        "properties": {
-          "pair": {
-            "type": "string"
-          },
-          "theo": {
-            "type": "number"
-          },
-          "minPrice": {
-            "type": "number"
-          },
-          "maxPrice": {
-            "type": "number"
-          },
-          "minEdge": {
-            "type": "number"
-          },
-          "edgeStep": {
-            "type": "number"
-          },
-          "ordersPerSide": {
-            "type": "integer"
-          },
-          "expires": {
-            "type": "integer"
-          }
-        },
-        "additionalProperties": false,
-        "required": [
-          "pair",
-          "theo",
-          "minEdge",
-          "edgeStep",
-          "ordersPerSide",
-          "expires"
-        ]
+  "type": "array",
+  "items": {
+    "type": "object",
+    "displayProperty": "pair",
+    "properties": {
+      "pair": {
+        "title": "Pair",
+        "description": "The pair name should match what you see on EtherDelta, for example MKR/ETH",
+        "type": "string"
+      },
+      "theo": {
+        "title": "Neutral point",
+        "description": "Neutral point for the price",
+        "type": "number"
+      },
+      "minPrice": {
+        "title": "Minimum price",
+        "description": "Minimum possible price (typically 0)",
+        "type": "number"
+      },
+      "maxPrice": {
+        "title": "Maximum price",
+        "description": "Maximum possible price (may be 1.00 for some prediction market tokens)",
+        "type": "number"
+      },
+      "minEdge": {
+        "title": "Minimum edge",
+        "description": "Minimum percent edge away from your neutral point (0.01 = 1%)",
+        "type": "number"
+      },
+      "edgeStep": {
+        "title": "Edge step",
+        "description": "Percent edge to widen for additional levels (0.01 = 1%)",
+        "type": "number"
+      },
+      "ordersPerSide": {
+        "title": "Orders per side",
+        "description": "Number of orders to insert on the buy side and sell side",
+        "type": "integer"
+      },
+      "expires": {
+        "title": "Expires",
+        "description": "Number of blocks until your orders expire",
+        "type": "integer"
       }
     },
-  },
-  "required": [
-    "pairs"
-  ]
+    "additionalProperties": false,
+    "required": [
+      "pair",
+      "theo",
+      "minEdge",
+      "edgeStep",
+      "ordersPerSide",
+      "expires"
+    ]
+  }
 };
+
+// var schema = {
+//   "$schema": "http://json-schema.org/draft-04/schema#",
+//   "type": "object",
+//   "additionalProperties": false,
+//   "properties": {
+//     "pairs": {
+//       "type": "array",
+//       "items": {
+//         "type": "object",
+//         "displayProperty": "pair",
+//         "properties": {
+//           "pair": {
+//             "title": "Pair",
+//             "description": "The pair name should match what you see on EtherDelta, for example MKR/ETH",
+//             "type": "string"
+//           },
+//           "theo": {
+//             "title": "Neutral point",
+//             "description": "Neutral point for the price",
+//             "type": "number"
+//           },
+//           "minPrice": {
+//             "title": "Minimum price",
+//             "description": "Minimum possible price (typically 0)",
+//             "type": "number"
+//           },
+//           "maxPrice": {
+//             "title": "Maximum price",
+//             "description": "Maximum possible price (may be 1.00 for some prediction market tokens)",
+//             "type": "number"
+//           },
+//           "minEdge": {
+//             "title": "Minimum edge",
+//             "description": "Minimum percent edge away from your neutral point (0.01 = 1%)",
+//             "type": "number"
+//           },
+//           "edgeStep": {
+//             "title": "Edge step",
+//             "description": "Percent edge to widen for additional levels (0.01 = 1%)",
+//             "type": "number"
+//           },
+//           "ordersPerSide": {
+//             "title": "Orders per side",
+//             "description": "Number of orders to insert on the buy side and sell side",
+//             "type": "integer"
+//           },
+//           "expires": {
+//             "title": "Expires",
+//             "description": "Number of blocks until your orders expire",
+//             "type": "integer"
+//           }
+//         },
+//         "additionalProperties": false,
+//         "required": [
+//           "pair",
+//           "theo",
+//           "minEdge",
+//           "edgeStep",
+//           "ordersPerSide",
+//           "expires"
+//         ]
+//       }
+//     },
+//   },
+//   "required": [
+//     "pairs"
+//   ]
+// };
 
 module.exports = schema;
