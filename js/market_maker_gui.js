c7ec5ab861665e5edb115db5096499db5a21eb81
Zack Coburn <zack@zackcoburn.com>
2016-11-15 01:13:00 -0500

    update

--- js/market_maker_gui.js
+++ js/market_maker_gui.js
@@ -2213,15 +2213,10 @@ var config = {
     privateKey: ''
   },
   pairs: [
-    {
-      pair: 'TRMPN/ETH', expires: 15000,
-      sellEnabled: 'onOrders/balance<0.25', sellNum: 5, sellVolume: '(balance - onOrders) / n * (0.9 + Math.random()*0.1)', sellPrice: '0.9 + 0.099 * i / n',
-      buyEnabled: 'onOrders/balance<0.25', buyNum: 5, buyVolume: '(balance - onOrders) / n / price * (0.9 + Math.random()*0.1)', buyPrice: '0.7 - 0.21 * i / n'
-    },
     // {
-    //   pair: 'TRMPY/ETH', expires: 25,
-    //   sellEnabled: 'onOrders/balance<0.25', sellNum: 15, sellVolume: '(balance - onOrders) / n * (0.9 + Math.random()*0.1)', sellPrice: '0.2 + 0.2 * i / n',
-    //   buyEnabled: 'onOrders/balance<0.25', buyNum: 15, buyVolume: '(balance - onOrders) / n / price * (0.9 + Math.random()*0.1)', buyPrice: '0.1 - 0.095 * i / n'
+    //   pair: 'TRMPN/ETH', expires: 15000,
+    //   sellEnabled: 'onOrders/balance<0.25', sellNum: 5, sellVolume: '(balance - onOrders) / n * (0.9 + Math.random()*0.1)', sellPrice: '0.9 + 0.099 * i / n',
+    //   buyEnabled: 'onOrders/balance<0.25', buyNum: 5, buyVolume: '(balance - onOrders) / n / price * (0.9 + Math.random()*0.1)', buyPrice: '0.7 - 0.21 * i / n'
     // },
   ],
 };
