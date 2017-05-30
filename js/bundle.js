a1a5520c001044e21353d58cf8f2a77c6f2dea04
Zack Coburn <zack@zackcoburn.com>
2017-05-30 10:15:49 -0400

    update

--- js/bundle.js
+++ js/bundle.js
@@ -1272,7 +1272,7 @@ module.exports = {
     // { addr: '0x1a95b271b0535d15fa49932daba31ba612b52946', name: 'MNE', decimals: 8 },
     { addr: '0xff3519eeeea3e76f1f699ccce5e23ee0bdda41ac', name: 'BCAP', decimals: 0 },
     { addr: '0x0d8775f648430679a709e98d2b0cb6250d2887ef', name: 'BAT', decimals: 18 },
-    { addr: '0xe7e01babb53c98f8d84d05dfff64ebb63ed37bf5', name: 'MYST', decimals: 0 },
+    { addr: '0xa645264c5603e96c3b0b078cdab68733794b0a71', name: 'MYST', decimals: 1 },
   ],
   defaultPair: { token: 'PLU', base: 'ETH' },
   pairs: [
