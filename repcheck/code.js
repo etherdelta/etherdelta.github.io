c2345061bdcec3f4948e5be90fcb0418f648c3d1
Zack Coburn <zack@zackcoburn.com>
2017-11-17 00:12:55 -0600

    update

--- repcheck/code.js
+++ repcheck/code.js
@@ -1,10 +1,11 @@
-function checker () {
-  const name = $('#name')[0].value;
+/* global $ */
+
+function checker() { // eslint-disable-line
+  const name = $('#name')[0].value.replace(/[^A-Za-z0-9_]/gi, '');
   const validNames = ['EtherDeltaZack_twitter', 'EtherDeltaRep1_twitter', 'EtherDeltaRep2_twitter', 'EtherDeltaRep3_twitter', 'EtherDeltaRep4_twitter', 'EtherDeltaUX_twitter'];
   if (validNames.map(x => x.toUpperCase()).indexOf(name.toUpperCase()) >= 0) {
     $('#result').html(`${name.toUpperCase()} is an official EtherDelta representative.`);
-  }
-  else {
+  } else {
     $('#result').html(`${name.toUpperCase()} is NOT OFFICIAL.`);
   }
 }
