17f3e4b0fbd5a57686d47190fb99625a10d870a3
SOMIDAX <23489369+somidax@users.noreply.github.com>
2018-03-23 03:33:54 +0100

    Update code.js

--- repcheck/code.js
+++ repcheck/code.js
@@ -2,9 +2,9 @@
 
 function checker() { // eslint-disable-line
   const name = $('#name')[0].value.replace(/[^A-Za-z0-9_]/gi, '');
-  const validNames = ['EtherDeltaZack_twitter', 'EtherDeltaRep1_twitter', 'EtherDeltaRep2_twitter', 'EtherDeltaRep3_twitter', 'EtherDeltaRep4_twitter', 'EtherDeltaUX_twitter'];
+  const validNames = ['coinEstate', 'SOMIDAXRep1_twitter', 'SOMIDAXRep2_twitter', 'SOMIDAXRep3_twitter', 'SOMIDAXRep4_twitter', 'SOMIDAXvoBit_twitter'];
   if (validNames.map(x => x.toUpperCase()).indexOf(name.toUpperCase()) >= 0) {
-    $('#result').html(`${name.toUpperCase()} is an official EtherDelta representative.`);
+    $('#result').html(`${name.toUpperCase()} is an official RYXEX representative.`);
   } else {
     $('#result').html(`${name.toUpperCase()} is NOT OFFICIAL.`);
   }
